39951478c380c7613a38fe129ab4a7d5758fb19f
g13913ks <g13913ks@m31307.tsuda.ac.jp>
2015-05-08 14:31:54 +0900

    ドライバー:片貝　ナビゲーター:長浦　テストケースを作成

--- test/inttoeng/test.java
+++ test/inttoeng/test.java
@@ -1,5 +1,6 @@
 package inttoeng;
 
+import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.*;
 
 import org.junit.Test;
@@ -7,11 +8,18 @@ import org.junit.Test;
 public class test {
 
 	@Test
-	public void 0‚ð“ü—Í‚·‚é() {
+	public void 0‚ð“ü—Í() {
 		IntToEng1 ite1 = new IntToEng1();
 		String expected = "zero";
-		String actual = ite1.translateEng("0");
+		String actual = ite1.translateEng(0);
 		assertThat(actual, is(expected));
+	}
+	@Test
+	public void 15‚ð“ü—Í(){
+		IntToEng1 ite1 = new IntToEng1();
+		String expected = "fifteen";
+		String actual = ite1.translateEng(15);
+		assertThat(actual,is(expected));
 	}
 
 }
